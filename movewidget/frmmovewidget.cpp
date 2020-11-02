7b9079d2799f9480b81a69f111c379161f476475
chenshijie <chenshijie@uniontech.com>
2020-11-02 19:07:49 +0800

    add rightbutton only can dray

--- movewidget/frmmovewidget.cpp
+++ movewidget/frmmovewidget.cpp
@@ -24,18 +24,29 @@ void frmMoveWidget::initForm()
     btn1->setText("按住我拖动(仅限左键拖动)");
     MoveWidget *moveWidget1 = new MoveWidget(this);
     moveWidget1->setWidget(btn1);
+    moveWidget1->setLeftButton(true);
+    moveWidget1->setRightButton(false);
 
     QPushButton *btn2 = new QPushButton(this);
     btn2->setGeometry(10, 40, 250, 25);
-    btn2->setText("按住我拖动(支持右键拖动)");
+    btn2->setText("按住我拖动(仅限右键拖动)");
     MoveWidget *moveWidget2 = new MoveWidget(this);
     moveWidget2->setWidget(btn2);
     moveWidget2->setLeftButton(false);
+    moveWidget2->setRightButton(true);
+
+    QPushButton *btn3 = new QPushButton(this);
+    btn3->setGeometry(10, 70, 250, 25);
+    btn3->setText("按住我拖动(支持左右键拖动)");
+    MoveWidget *moveWidget3 = new MoveWidget(this);
+    moveWidget3->setWidget(btn3);
+    moveWidget3->setLeftButton(true);
+    moveWidget3->setRightButton(true);
 
     QProgressBar *bar = new QProgressBar(this);
-    bar->setGeometry(10, 70, 250, 25);
+    bar->setGeometry(10, 100, 250, 25);
     bar->setRange(0, 0);
     bar->setTextVisible(false);
-    MoveWidget *moveWidget3 = new MoveWidget(this);
-    moveWidget3->setWidget(bar);
+    MoveWidget *moveWidget4 = new MoveWidget(this);
+    moveWidget4->setWidget(bar);
 }
--- movewidget/movewidget.h
+++ movewidget/movewidget.h
@@ -34,12 +34,15 @@ private:
     QPoint lastPoint;   //最后按下的坐标
     bool pressed;       //鼠标是否按下
     bool leftButton;    //限定鼠标左键
+    bool rightButton;   //限定鼠标右键
     bool inControl;     //限定在容器内
     QWidget *widget;    //移动的控件
 
 public Q_SLOTS:
     //设置是否限定鼠标左键
     void setLeftButton(bool leftButton);
+    //设置是否限定鼠标右键
+    void setRightButton(bool rightButton);
     //设置是否限定不能移出容器外面
     void setInControl(bool inControl);
     //设置要移动的控件
