366d7d8416408a3c0ce8e1b651606b4ee68edfea
tenghuan <tenghuan@uniontech.com>
2021-05-31 15:46:13 +0800

    feat: 增加图片验证功能
     增加图片验证功能
    
    Log: 增加图片验证功能

---
+++ slidepuzzlewidget/main.cpp
@@ -0,0 +1,30 @@
+﻿#pragma execution_character_set("utf-8")
+
+#include "frmslidepuzzlewidget.h"
+#include <QApplication>
+#include <QTextCodec>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	a.setFont(QFont("Microsoft Yahei", 10));
+
+#if (QT_VERSION <= QT_VERSION_CHECK(5,0,0))
+#if _MSC_VER
+	QTextCodec *codec = QTextCodec::codecForName("gbk");
+#else
+	QTextCodec *codec = QTextCodec::codecForName("utf-8");
+#endif
+	QTextCodec::setCodecForLocale(codec);
+	QTextCodec::setCodecForCStrings(codec);
+	QTextCodec::setCodecForTr(codec);
+#else
+	QTextCodec *codec = QTextCodec::codecForName("utf-8");
+	QTextCodec::setCodecForLocale(codec);
+#endif
+
+	FrmSlidePuzzleWidget w;
+	w.setWindowTitle("滑块图片验证码");
+	w.show();
+	return a.exec();
+}
