259a8def434d8fdd7901531c7516e0fa94dd90ae
feiyangqingyun <feiyangqingyun@163.com>
2023-06-25 20:03:46 +0800

    更新代码

--- video/plaympv/widget.cpp
+++ video/plaympv/widget.cpp
@@ -7,9 +7,8 @@ Widget::Widget(QWidget *parent) : QWidget(parent), ui(new Ui::Widget)
     ui->setupUi(this);
 
     QStringList urls;
-    urls << "https://hls01open.ys7.com/openlive/6e0b2be040a943489ef0b9bb344b96b8.hd.m3u8";
-    urls << "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov";
-    urls << "http://vfx.mtime.cn/Video/2019/02/04/mp4/190204084208765161.mp4";
+    urls << "http://vfx.mtime.cn/Video/2021/11/16/mp4/211116131456748178.mp4";
+    urls << "http://vd3.bdstatic.com/mda-jennyc5ci1ugrxzi/mda-jennyc5ci1ugrxzi.mp4";
     urls << "rtsp://admin:Admin123456@192.168.0.15:554/media/video1";
     ui->cboxUrl->addItems(urls);
     ui->cboxUrl->setCurrentIndex(0);
